a159f29932109578a06fa55b5d5cdd75c0366c55
zeeket <emil.heliste@helsinki.fi>
2024-07-19 09:00:39 +0300

    try to use test reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['html'], ['@estruyf/github-actions-reporter']]
+    : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
