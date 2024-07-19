b65bd4c3366175694d9d8ed746436feddb5d5724
zeeket <emil.heliste@helsinki.fi>
2024-07-19 08:20:10 +0300

    fix test workflow, split tests

--- tests/rental.spec.ts
+++ tests/rental.spec.ts
@@ -1,12 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/HYTKY/);
-});
-
 test("navigate to rental page and back", async ({ page }) => {
   await page.goto("/");
 
