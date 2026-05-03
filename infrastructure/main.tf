7ffb419132d4150590c2c9fa0a837498ef274843
zeeket <emil.heliste@helsinki.fi>
2026-05-03 22:46:52 +0300

    feat(security): add DO firewall, nginx security headers, and CI hardening
    - Add DigitalOcean Cloud Firewall (ports 22/80/443 inbound, all outbound)
    - Add X-Content-Type-Options, X-Frame-Options, Referrer-Policy to nginx HTTP block
    - Add same headers plus HSTS (max-age=31536000) to nginx HTTPS block
    - Add post-apply SSH and web health checks to push-apply workflow
    - Restrict test, lint, and prettier workflows to app code changes via paths-ignore

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -83,6 +83,46 @@ resource "digitalocean_droplet" "webserver" {
   }
 }
 
+resource "digitalocean_firewall" "webserver" {
+  name        = "webserver-firewall"
+  droplet_ids = [digitalocean_droplet.webserver.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 resource "digitalocean_domain" "hytky" {
   name = "hytky.org"
 }
